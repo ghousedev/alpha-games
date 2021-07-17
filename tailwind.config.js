d82247afa60ddc137171d574baafcebaed54ab30
Rob G <crx8490@gmail.com>
2021-07-17 08:05:10 +0100

    add custom z-index

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    zIndex: {
+      '1500': 1500
+    },
     extend: {},
   },
   variants: {
