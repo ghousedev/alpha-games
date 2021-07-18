a26dedc78acf5e547986591ce4b0bc410b77e7be
Rob G <crx8490@gmail.com>
2021-07-18 09:25:39 +0100

    add flex classes

--- components/layout.js
+++ components/layout.js
@@ -3,9 +3,9 @@ import Footer from "./footer";
 
 export default function Layout( {children} ) {
     return (
-        <div>
+        <div className="min-h-screen bg-gray-800 flex flex-col">
             <Header />
-                <main className="w-full bg-gray-800 z-40">{children}</main>
+                <main className="w-full bg-gray-800 z-40 flex-grow">{children}</main>
             <Footer />
         </div>
     )
