34032f59d88332509fcde5c0c23a2819cfdb5859
Rob G <crx8490@gmail.com>
2021-07-18 09:26:10 +0100

    formatting

--- pages/index.js
+++ pages/index.js
@@ -11,18 +11,18 @@ import Contact from '../components/contact'
 import Footer from '../components/footer'
 
 // Parse date and time string into time since unix epoch in ms
-function epoch(date){
+function epoch(date) {
   return Date.parse(date)
 }
 
-export default function Home({...props}) {
+export default function Home({ ...props }) {
   // Destructure props for use
   let events = [...props.events]
   let previousevents = [...props.previousevents]
   let posts = [...props.posts]
 
   // Sort events into soonest first
-  events = events.sort((a, b) => 
+  events = events.sort((a, b) =>
     parseInt(epoch(a.end_time)) - parseInt(epoch(b.end_time))
   )
 
@@ -32,57 +32,57 @@ export default function Home({...props}) {
   )
 
   // Set scroll margin by header height
-
+  // TODO
 
   return (
-    <div style={{scrollSnapType: 'y mandatory', scrollBehavior: 'smooth'}}>
+    <div style={{ scrollSnapType: 'y mandatory', scrollBehavior: 'smooth' }}>
       <Head>
         <title>Alpha Games</title>
         <link rel="icon" href="/alphaicon.svg" />
       </Head>
-      <Hero/>
-      <section id="news" style={{scrollMarginTop: 112, scrollSnapAlign: 'start'}}>
-      <Divider text="LATEST NEWS"/>
-      <CardCarousel content={posts}/>
+      <Hero />
+      <section id="news" style={{ scrollMarginTop: 112, scrollSnapAlign: 'start' }}>
+        <Divider text="LATEST NEWS" />
+        <CardCarousel content={posts} />
       </section>
-      <section id="events" style={{scrollMarginTop: 112, scrollSnapAlign: 'start'}}>
-      <Divider text="UPCOMING EVENTS"/>
-      <EventCarousel content={events}/>
-      <Divider text="PREVIOUS EVENTS"/>
-      <PreviousCarousel content={previousevents}/>
+      <section id="events" style={{ scrollMarginTop: 112, scrollSnapAlign: 'start' }}>
+        <Divider text="UPCOMING EVENTS" />
+        <EventCarousel content={events} />
+        <Divider text="PREVIOUS EVENTS" />
+        <PreviousCarousel content={previousevents} />
       </section>
-      <section id="about" style={{scrollMarginTop: 112, scrollSnapAlign: 'start'}}>
-      <Divider text="ABOUT US"/>
-      <About/>
+      <section id="about" style={{ scrollMarginTop: 112, scrollSnapAlign: 'start' }}>
+        <Divider text="ABOUT US" />
+        <About />
       </section>
-      <section id="contact" style={{scrollMarginTop: 112, scrollSnapAlign: 'start'}}>
-      <Divider text="CONTACT"/>
-      <Contact/>
+      <section id="contact" style={{ scrollMarginTop: 112, scrollSnapAlign: 'start' }}>
+        <Divider text="CONTACT" />
+        <Contact />
       </section>
     </div>
-          )
+  )
 }
 
 // Get data needed to fill components from mongodb instance
 export async function getServerSideProps() {
   const { db } = await connectToDatabase();
   const posts = await db
-      .collection("posts")
-      .find({})
-      .toArray();
+    .collection("posts")
+    .find({})
+    .toArray();
   const events = await db
-      .collection("events")
-      .find({})
-      .toArray()
+    .collection("events")
+    .find({})
+    .toArray()
   const previousevents = await db
-      .collection("previousevents")
-      .find({})
-      .toArray()
+    .collection("previousevents")
+    .find({})
+    .toArray()
   return {
-      props: {
-          posts: posts,
-          events: events,
-          previousevents: previousevents
-      },
+    props: {
+      posts: posts,
+      events: events,
+      previousevents: previousevents
+    },
   };
 }
