95eaa1756f40990e3d4b095dad3c4be4a2ef70bb
Rob G <crx8490@gmail.com>
2021-07-19 12:17:33 +0100

    basic page

--- pages/privacy.js
+++ pages/privacy.js
@@ -1,3 +1,15 @@
+import Head from "next/head"
+
 export default function Privacy() {
-    return (null)
+    return (
+        <div className="flex flex-col">
+            <Head>
+                <title>ALpha Games privacy policy</title>
+                <link rel="icon" href="/alphaicon.svg" />
+            </Head>
+            <div className="overflow-hidden shadow-lg rounded-lg border-2 border-yellow-300 bg-gray-600 lg:w-2/3 mx-auto flex-grow">
+                <p className="mx-2 my-2 text-gray-300 font-medium text-md">Privacy policy</p>
+            </div>
+        </div>
+    )
 }
