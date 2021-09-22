7d0fa20192b6b5fe3bda6d8e8b6e83c3b7d03f7f
Rob G <crx8490@gmail.com>
2021-09-22 14:26:43 +0100

    loading spinner component

---
+++ components/loading.js
@@ -0,0 +1,9 @@
+import CircleLoader from 'react-spinners/CircleLoader'
+
+export default function Loading() {
+    return (
+       <div className="h-60">
+            <CircleLoader />
+       </div>
+    )
+}
