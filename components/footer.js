c0345d41ae45006320fa9f6b3c45333f9cfa89b5
Rob G <crx8490@gmail.com>
2021-09-22 12:27:40 +0100

    add descriptive text to facebook link

--- components/footer.js
+++ components/footer.js
@@ -45,10 +45,11 @@ export default function Footer() {
       <a href="https://ghouse.dev" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">GHouse Developments</a>
     </p>
     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-      <a className="text-gray-500" href="https://facebook.com/alphagamesuk">
-        <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5" viewBox="0 0 24 24">
+      <a className="text-gray-500" href="https://facebook.com/alphagamesuk" rel="nofollow">
+        <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} className="w-5 h-5 m-auto" viewBox="0 0 24 24">
           <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
         </svg>
+        Facebook
       </a>
     </span>
   </div>
