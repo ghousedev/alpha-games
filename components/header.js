7423970a6a0b8b1705f1c6d5e6dc1750aff51b99
Rob G <crx8490@gmail.com>
2021-09-23 09:14:53 +0100

    better contrast text color

--- components/header.js
+++ components/header.js
@@ -5,20 +5,20 @@ export default function Header() {
 
   return (
     <div className="w-full sticky top-0 z-1500">
-      <header className="text-gray-600 body-font bg-yellow-400">
+      <header className="text-gray-800 body-font bg-yellow-400">
         <div className="container mx-auto flex flex-wrap p-4 pb-2 flex-col md:flex-row items-center">
           <nav className="flex lg:w-2/5 flex-wrap items-center text-sm sm:text-lg md:ml-auto font-semibold">
-            <div className="mr-5 hover:text-gray-900">
+            <div className="mr-5 hover:text-gray-600">
               <Link href="/#news">News</Link>
             </div>
-            <div className="mr-5 hover:text-gray-900">
-              <Link className="mr-5 hover:text-gray-900" href="/#events">Events</Link>
+            <div className="mr-5 hover:text-gray-600">
+              <Link href="/#events">Events</Link>
             </div>
-            <div className="mr-5 hover:text-gray-900">
-              <Link className="mr-5 hover:text-gray-900" href="/#about">About</Link>
+            <div className="mr-5 hover:text-gray-600">
+              <Link href="/#about">About</Link>
             </div>
-            <div className="mr-5 hover:text-gray-900">
-              <Link className="hover:text-gray-900" href="/#contact">Contact</Link>
+            <div className="mr-5 hover:text-gray-600">
+              <Link href="/#contact">Contact</Link>
             </div>
           </nav>
           <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-0 md:mb-0" href="https://www.alphagames.co.uk">
@@ -59,7 +59,7 @@ export default function Header() {
                 </g>
               </g>
             </svg>
-            <span className="ml-3 text-md sm:text-xl">Alpha Games</span>
+            <span className="ml-3 text-md sm:text-xl hover:text-gray-600">Alpha Games</span>
           
           </a>
           <div className="lg:w-2/5 inline-flex lg:justify-end sm:ml-2 lg:ml-0">
