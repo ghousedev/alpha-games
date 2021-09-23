8ab2ef6587e34bd5692fa118139cbbec572461c7
Rob G <crx8490@gmail.com>
2021-09-23 09:16:19 +0100

    don't use full width past 3xl breakpoint

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,7 @@ export default function Layout( {children} ) {
     return (
         <div className="min-h-screen bg-gray-800 flex flex-col">
             <Header />
-                <main className="w-full bg-gray-800 z-40 flex-grow">{children}</main>
+                <main className="w-full bg-gray-800 z-40 flex-grow 3xl:w-3/4 3xl:m-auto">{children}</main>
             <Footer />
         </div>
     )
