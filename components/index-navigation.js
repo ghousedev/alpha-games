91bf531e402e80a85f09996e80727848ba1c2731
Rob G <crx8490@gmail.com>
2021-09-27 10:09:13 +0100

    fix scrolling to elements

--- components/index-navigation.js
+++ components/index-navigation.js
@@ -1,41 +1,16 @@
 import Link from 'next/link';
-import { useEffect } from 'react';
 import LogoSVG from './logo-svg';
 
 export default function IndexNavigation() {
 
-    let newsY;
-    let eventsY;
-    let aboutY;
-    let contactY;
-
-    useEffect(() => {
-        const newsElement = document.getElementById('news');
-        if (newsElement !== null) {
-            newsY = newsElement.getBoundingClientRect().top;
-        }
-        const eventsElement = document.getElementById('events');
-        if (eventsElement !== null) {
-            eventsY = eventsElement.getBoundingClientRect().top;
-        }
-        const aboutElement = document.getElementById('about');
-        if (aboutElement !== null) {
-            aboutY = aboutElement.getBoundingClientRect().top;
-        }
-        const contactElement = document.getElementById('contact');
-        if (contactElement !== null) {
-            contactY = contactElement.getBoundingClientRect().top;
-        }
-    });
-
-    const scrollToElement = (y) => {
+    const scrollToElement = (selector) => {
+        const elementY = document.getElementById(selector).getBoundingClientRect().top;
         const header = document.querySelector('.header');
-        let headerHeight = header.offsetHeight;
-        const offset = headerHeight;
-        const yPos = y - offset;
-        window.scrollTo({ top: yPos, behavior: 'smooth' });
+        const offset = header.offsetHeight;
+        const yPos = elementY - offset;
+        const scrollY = window.scrollY;
+        window.scrollTo({ top: yPos + scrollY, behavior: 'smooth' });
     }
-
 
     return (
         <div className="w-full sticky top-0 z-1500">
@@ -44,16 +19,16 @@ export default function IndexNavigation() {
                     <nav className="flex lg:w-2/5 flex-wrap items-center text-sm sm:text-lg md:ml-auto font-semibold">
 
                         <div className="mr-5 hover:text-gray-600">
-                            <button className="font-semibold" onClick={() => scrollToElement(newsY)}>News</button>
+                            <button className="font-semibold" onClick={() => scrollToElement('news')}>News</button>
                         </div>
                         <div className="mr-5 hover:text-gray-600">
-                            <button className="font-semibold" onClick={() => scrollToElement(eventsY)}>Events</button>
+                            <button className="font-semibold" onClick={() => scrollToElement('events')}>Events</button>
                         </div>
                         <div className="mr-5 hover:text-gray-600">
-                            <button className="font-semibold" onClick={() => scrollToElement(aboutY)}>About</button>
+                            <button className="font-semibold" onClick={() => scrollToElement('about')}>About</button>
                         </div>
                         <div className="mr-5 hover:text-gray-600">
-                            <button className="font-semibold" onClick={() => scrollToElement(contactY)}>Contact</button>
+                            <button className="font-semibold" onClick={() => scrollToElement('contact')}>Contact</button>
                         </div>
                     </nav>
                     <div className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-0 md:mb-0">
